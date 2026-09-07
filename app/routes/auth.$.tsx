60933b2be16cfaf0d23408e921718b709635ebff
Smart COD Build <build@smartcod.local>
2026-09-07 16:42:12 +0530

    Fix auth.$.tsx to call login() for /auth/login routes

--- app/routes/auth.$.tsx
+++ app/routes/auth.$.tsx
@@ -1,7 +1,21 @@
-import type { LoaderFunctionArgs } from "@remix-run/node";
-import { authenticate } from "../shopify.server";
+import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
+import { authenticate, login } from "../shopify.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  if (url.pathname.includes("/login")) {
+    return await login(request);
+  }
   await authenticate.admin(request);
   return null;
 };
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const url = new URL(request.url);
+  if (url.pathname.includes("/login")) {
+    return await login(request);
+  }
+  await authenticate.admin(request);
+  return null;
+};
+
